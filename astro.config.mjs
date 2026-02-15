e16a074c9db7cce06d18136bd6b4d935a9f18891
dr1zzle-bit <matthias@michelic.com>
2026-02-15 20:05:39 +0100

    chore: add @astrojs/netlify adapter for SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,28 @@ import { sanityConfig } from './src/utils/sanity-client';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        domains: ['cdn.sanity.io']
-    },
-    integrations: [sanity(sanityConfig), react()],
-    vite: {
-        plugins: [tailwindcss()],
-        server: {
-            hmr: { path: '/vite-hmr/' },
-            allowedHosts: ['.netlify.app']
-        }
-    },
-    server: {
-        port: 3000
-    },
-    prefetch: true
+  image: {
+      domains: ['cdn.sanity.io']
+  },
+
+  integrations: [sanity(sanityConfig), react()],
+
+  vite: {
+      plugins: [tailwindcss()],
+      server: {
+          hmr: { path: '/vite-hmr/' },
+          allowedHosts: ['.netlify.app']
+      }
+  },
+
+  server: {
+      port: 3000
+  },
+
+  prefetch: true,
+  adapter: netlify()
 });
